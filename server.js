295612e7c82afbadbf2082a7e866a9b0a0d334bd
Muhammad Asfandyar Ashraf <AshraAsf@corp.contoursoftware.net>
2023-03-15 21:56:20 +0500

    fourth commit

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var cors = require('cors');
 const db = require('./config/db');
 const JWT = require('jsonwebtoken')
 const User = require("./Model/User")
-const Exercise = require("./Model/Exercise")
+const Exercise = require("./Model/Exercise");
+const { find } = require('./Model/User');
 
 db();
 const secret = "lqmxBi8W65uubKPk9o8fC3azvzhCg6Jl";
@@ -41,26 +42,30 @@ app.use("/home",(req,res,next)=>{
 
 
 
-app.post("/register",async (req,res)=>{
+app.post("/signup",async (req,res)=>{
   const {name, email, password} = req.body
+  console.log(req.body)
 
   try{
 
   const result = await  User.create({name,email,password})
   
-  res.status(200).send({
+
+  res.status(200).json({
       message:"Register Successful",
       User:result
   })
   
 }
 catch(err){
-  res.send("User Not Created" )
+  res.json("User Not Created" )
 
 }
 })
 
 
+
+// Login user
 app.post("/login",async (req,res)=>{
   
   const {email,password} = req.body
@@ -70,16 +75,44 @@ app.post("/login",async (req,res)=>{
 
 
   if(result==null){
-      res.status(401).json("Invalid Credentials")
+      res.status(401).json({message: "Invalid Credentials"})
   }
   else{
     const Token = JWT.sign({id:result["_id"]},secret)
-    res.cookie("token",Token,{
-      httpOnly:true,
-    })
-    res.status(200).json("Logged In");
+
+    res.status(200).json({ token: Token, message:"Logged in"});
       
   }
+})
+
+
+
+
+
+app.get("/user", async (req,res) =>
+{
+
+  let token = req.headers.token;
+  // token = token.slice(7);
+  console.log(token)
+
+  JWT.verify(token, secret , async(err,data)=> {
+    if(err){
+        res.status(401).send("Not Verified")
+    }
+
+    else{
+
+      const result  = await User.find({_id:data.id}) 
+      if(result==null){
+        res.status(401).json({message: "No User Found"})
+       }
+      else{
+      res.status(200).json({User: result, message:"User Found"})
+    }
+   }
+  })
+
 })
 
 
